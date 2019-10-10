82a061f64406bf2180414ce29fc7eec9299422ca
erikkjernlie <erik.kjernlie@gmail.com>
2019-10-10 19:57:50 +0200

    Add training in browser

--- src/Routes/Project/TrainModel.js
+++ src/Routes/Project/TrainModel.js
@@ -13,6 +13,7 @@ import { csv } from "d3";
 import * as tf from "@tensorflow/tfjs";
 import * as tfvis from "@tensorflow/tfjs-vis";
 import * as data from "./data";
+import getProcessedData from "./processData";
 
 const TrainModel = ({ match }) => {
   const models = useModels();
@@ -27,28 +28,61 @@ const TrainModel = ({ match }) => {
   useEffect(() => {
     csv("/rig_good.csv").then(data => {
       let sensorNames = Object.keys(data[0]);
-      console.log(sensorNames);
-      let dataPoints = data.slice(1);
       setSensorNames(sensorNames);
-      setDatapoints(dataPoints);
-      console.log(dataPoints);
-      train();
+      setDatapoints(data);
+
+      train(data);
     });
 
+    /*
     const dataset = tf.data.csv(
       "https://firebasestorage.googleapis.com/v0/b/tpk4450-project.appspot.com/o/rig_good.csv?alt=media&token=9792ce1c-7196-4a0d-80c2-f83ad35d7744"
     );
     const dataset2 = tf.data.csv("./rig_good.csv");
     console.log("TF DATA", dataset2);
+    */
   }, []);
 
-  async function train() {
-    console.log("train model");
-    const [xTrain, yTrain, xTest, yTest] = data.getIrisData(0.15);
-    console.log(xTrain, xTest);
+  async function train(data) {
+    console.log(data);
+    const dataset = data.map(x => Object.values(x).map(Number));
 
-    const model = await trainModel(xTrain, yTrain, xTest, yTest);
-    console.log("PREDICT", model.predict(xTest));
+    // const [xTrain, yTrain, xTest, yTest] = getProcessedData(dataset, 0.15);
+    const numberOfColumns = Object.keys(data[0]).length;
+    const numberOfRows = dataset.length;
+    console.log(dataset);
+    const features = dataset.map(x => x.slice(0, numberOfColumns - 1));
+    const targets = dataset.map(x => x.slice(numberOfColumns - 1));
+
+    const test_train_split = 0.2;
+
+    const numberOfTest = Math.round(numberOfRows * test_train_split);
+    const numberOfTrain = numberOfRows - numberOfTest;
+
+    const x_train = features.slice(0, numberOfTrain - 1);
+    const x_test = features.slice(numberOfTrain - 1);
+    const y_train = targets.slice(0, numberOfTrain - 1);
+    const y_test = targets.slice(numberOfTrain - 1);
+
+    const tensors = {};
+
+    tensors.trainFeatures = tf.tensor2d(x_train);
+    tensors.trainTargets = tf.tensor2d(y_train);
+    tensors.testFeatures = tf.tensor2d(x_test);
+    tensors.testTargets = tf.tensor2d(y_test);
+
+    // const [xTrain, yTrain, xTest, yTest] = data.getIrisData(0.15);
+    // console.log(xTrain, xTest);
+    console.log("X_TRAIN", x_train);
+    console.log("TENSORS", tensors);
+
+    const model = await trainModel(
+      tensors.trainFeatures,
+      tensors.trainTargets,
+      tensors.testFeatures,
+      tensors.testTargets
+    );
+    console.log("PREDICT", model.predict(tensors.trainFeatures));
   }
 
   async function trainModel(xTrain, yTrain, xTest, yTest) {
@@ -60,21 +94,26 @@ const TrainModel = ({ match }) => {
     model.add(
       tf.layers.dense({
         units: 10,
-        activation: "sigmoid",
+        activation: "relu",
         inputShape: [xTrain.shape[1]]
       })
     );
-    model.add(tf.layers.dense({ units: 3, activation: "softmax" }));
+    model.add(
+      tf.layers.dense({
+        units: 5,
+        activation: "relu"
+      })
+    );
+    model.add(tf.layers.dense({ units: 1, activation: "relu" }));
     model.summary();
 
     // learningrate
     const learningRate = 0.01;
-    const epochs = 100;
+    const epochs = 20;
     const optimizer = tf.train.adam(learningRate);
     model.compile({
       optimizer: optimizer,
-      loss: "categoricalCrossentropy",
-      metrics: ["accuracy"]
+      loss: "meanSquaredError"
     });
 
     const trainLogs = [];
@@ -90,6 +129,17 @@ const TrainModel = ({ match }) => {
       validationData: [xTest, yTest],
       callbacks: callbacks
     });
+    console.log("PREDICTING");
+
+    // 1261.0421142578125,27.090818405151367,4.955190658569336
+
+    model
+      .predict(tf.tensor2d([[1261.0421142578125, 27.090818405151367]], [1, 2]))
+      .print();
+
+    // SAVE MODEL TO FIRESTORE AND TO STORE IN APPLICATION SO IT CAN PREDICT ELSEWHERE
+
+    // model.predict(tf.tensor2d([[5.1111, 3.50004, 1.4303]], [1, 3])).print();
     // nconst secPerEpoch = (performance.now() - beginMs) / (1000 * epochs);
     return model;
   }
---
+++ src/Routes/Project/processData.js
@@ -0,0 +1,96 @@
+import * as tf from "@tensorflow/tfjs";
+
+export function getProcessedData(dataset, testSplit) {
+  // TODO: GENERALIZE FOR SEVERAL, SELECTED OUTPUTS
+  return tf.tidy(() => {
+    const dataByClass = [];
+
+    const classes = Object.keys(dataset[0]); // ["Load", "Dis..."]
+    const targetsByClass = [];
+    // const data = [{"Load": "sdasd", "Displacement": "aasd", ....}]
+
+    const newDataset = dataset.map(point => Object.values(point)); // [[0.1, 0.2, 0.4], [...]]
+
+    for (let i = 0; i < classes.length; ++i) {
+      dataByClass.push([]);
+      targetsByClass.push([]);
+    }
+    console.log(dataByClass);
+    console.log(targetsByClass);
+    for (const row of newDataset) {
+      console.log("ROW", row);
+      const target = row[row.length - 1];
+      const data = row.slice(0, row.length - 1);
+      dataByClass[target].push(data);
+      targetsByClass[target].push(target);
+    }
+
+    const xTrains = [];
+    const yTrains = [];
+    const xTests = [];
+    const yTests = [];
+    for (let i = 0; i < classes.length; ++i) {
+      const [xTrain, yTrain, xTest, yTest] = convertToTensors(
+        dataByClass[i],
+        targetsByClass[i],
+        testSplit,
+        classes.length
+      );
+      xTrains.push(xTrain);
+      yTrains.push(yTrain);
+      xTests.push(xTest);
+      yTests.push(yTest);
+    }
+
+    const concatAxis = 0;
+    return [
+      tf.concat(xTrains, concatAxis),
+      tf.concat(yTrains, concatAxis),
+      tf.concat(xTests, concatAxis),
+      tf.concat(yTests, concatAxis)
+    ];
+  });
+}
+
+function convertToTensors(data, targets, testSplit, numberOfClasses) {
+  const numExamples = data.length;
+  if (numExamples !== targets.length) {
+    throw new Error("data and split have different numbers of examples");
+  }
+
+  // Randomly shuffle `data` and `targets`.
+  const indices = [];
+  for (let i = 0; i < numExamples; ++i) {
+    indices.push(i);
+  }
+  tf.util.shuffle(indices);
+
+  const shuffledData = [];
+  const shuffledTargets = [];
+  for (let i = 0; i < numExamples; ++i) {
+    shuffledData.push(data[indices[i]]);
+    shuffledTargets.push(targets[indices[i]]);
+  }
+
+  // Split the data into a training set and a tet set, based on `testSplit`.
+  const numTestExamples = Math.round(numExamples * testSplit);
+  const numTrainExamples = numExamples - numTestExamples;
+
+  const xDims = shuffledData[0].length;
+
+  // Create a 2D `tf.Tensor` to hold the feature data.
+  const xs = tf.tensor2d(shuffledData, [numExamples, xDims]);
+
+  // Create a 1D `tf.Tensor` to hold the labels, and convert the number label
+  // from the set {0, 1, 2} into one-hot encoding (.e.g., 0 --> [1, 0, 0]).
+  const ys = tf.oneHot(tf.tensor1d(shuffledTargets).toInt(), numberOfClasses);
+
+  // Split the data into training and test sets, using `slice`.
+  const xTrain = xs.slice([0, 0], [numTrainExamples, xDims]);
+  const xTest = xs.slice([numTrainExamples, 0], [numTestExamples, xDims]);
+  const yTrain = ys.slice([0, 0], [numTrainExamples, numberOfClasses]);
+  const yTest = ys.slice([0, 0], [numTestExamples, numberOfClasses]);
+  return [xTrain, yTrain, xTest, yTest];
+}
+
+export default getProcessedData;
