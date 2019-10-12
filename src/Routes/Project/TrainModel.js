348471f25c03090ea4d386d06194c031ab755a5a
Herman Wika Horn <herman.wika.horn@entur.org>
2019-10-12 21:38:59 +0200

    implements normalization and standardization NB: pip install simple-statistics

--- src/Routes/Project/TrainModel.js
+++ src/Routes/Project/TrainModel.js
@@ -7,6 +7,16 @@ import {
 import "./TrainModel.css";
 import Sensor from "../../Components/Sensor/Sensor";
 import { Link } from "react-router-dom";
+import {
+  shuffle,
+  sampleCovariance,
+  max,
+  min,
+  sum,
+  mean,
+  standardDeviation,
+  sampleStandardDeviation,
+} from 'simple-statistics'
 
 import { csv } from "d3";
 
@@ -43,13 +53,12 @@ const TrainModel = ({ match }) => {
     */
   }, []);
 
-  function getRSquared(predict, data) {
+  function getR2Score(predict, data) {
     console.log("Inside getRSquared function")
     console.log(predict)
     console.log(data)
-    var yAxis = data.map(x => Number(x));
+    data = data.map(x => Number(x));
     predict = predict.map(x => Number(x))
-    var rPrediction = [];
 
     var meanValue = 0; // MEAN VALUE
     var SStot = 0; // THE TOTAL SUM OF THE SQUARES
@@ -57,19 +66,17 @@ const TrainModel = ({ match }) => {
     var rSquared = 0;
 
     // SUM ALL VALUES
-    for (var n = 0; n < yAxis.length; n++) { 
-      meanValue += yAxis[n];
+    for (var n = 0; n < data.length; n++) { 
+      meanValue += data[n];
     }
     // GET MEAN VALUE 
-    meanValue = (meanValue / yAxis.length);
-    
-    for (var n = 0; n < yAxis.length; n++) {
-      // CALCULATE THE SSTOTAL    
-      SStot += Math.pow(yAxis[n] - meanValue, 2); 
-      // REGRESSION PREDICTION
-      rPrediction.push(predict[n]);
+    meanValue = (meanValue / data.length);
+
+    for (var n = 0; n < data.length; n++) {
+      // CALCULATE THE SSTOTAL
+      SStot += Math.pow(data[n] - meanValue, 2); 
       // CALCULATE THE SSRES
-      SSres += Math.pow(rPrediction[n] - yAxis[n], 2);
+      SSres += Math.pow(predict[n] - data[n], 2);
     }
 
     // R SQUARED
@@ -83,39 +90,16 @@ const TrainModel = ({ match }) => {
     };
   }
 
-  function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-  
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-  
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-  
-    return array;
-  }
-
-  async function train(data) {
-    console.log(data);
-    let dataset = data.map(x => Object.values(x).map(Number));
-    dataset = shuffle(dataset)
-
-    // const [xTrain, yTrain, xTest, yTest] = getProcessedData(dataset, 0.15);
-    const numberOfColumns = Object.keys(data[0]).length;
-    const numberOfRows = dataset.length;
-    console.log(dataset);
+  function getFeatureTargetSplit(dataset) {
+    const numberOfColumns = Object.keys(dataset[0]).length;
+    
     const features = dataset.map(x => x.slice(0, numberOfColumns - 1));
     const targets = dataset.map(x => x.slice(numberOfColumns - 1));
+    return [features, targets]
+  }
 
-    const test_train_split = 0.2;
-
+  function getTestTrainSplit(features, targets, test_train_split) {
+    const numberOfRows = features.length;
     const numberOfTest = Math.round(numberOfRows * test_train_split);
     const numberOfTrain = numberOfRows - numberOfTest;
 
@@ -123,19 +107,75 @@ const TrainModel = ({ match }) => {
     const x_test = features.slice(numberOfTrain - 1);
     const y_train = targets.slice(0, numberOfTrain - 1);
     const y_test = targets.slice(numberOfTrain - 1);
+    return [x_train, x_test, y_train, y_test]
+  }
 
+  function convertToTensors(x_train, x_test, y_train, y_test) {
     const tensors = {};
-
     tensors.trainFeatures = tf.tensor2d(x_train);
     tensors.trainTargets = tf.tensor2d(y_train);
     tensors.testFeatures = tf.tensor2d(x_test);
     tensors.testTargets = tf.tensor2d(y_test);
+    return tensors
+  }
 
-    // const [xTrain, yTrain, xTest, yTest] = data.getIrisData(0.15);
-    // console.log(xTrain, xTest);
-    console.log("X_TRAIN", x_train);
-    console.log("TENSORS", tensors);
+  function normalizeData(data) {
+    const numberOfColumns = data[0].length
+    const numberOfRows = data.length
+    let maxvals = []
+    let minvals = []
+    for (var i = 0; i < numberOfColumns; i++) {
+      const col = data.map(x => x[i])
+      maxvals.push(max(col))
+      minvals.push(min(col))
+    }
+    const normalized = []
+    for (var i = 0; i < numberOfRows; i++) {
+      const row = []
+      for (var j = 0; j < numberOfColumns; j++) {
+        row.push((data[i][j] - minvals[j])/(maxvals[j]-minvals[j]))
+      }
+      normalized.push(row)
+    }
+    return normalized
+  }
 
+  function standardizeData(data) {
+    const numberOfColumns = data[0].length
+    const numberOfRows = data.length
+    let meanvals = []
+    let stdvals = []
+    for (var i = 0; i < numberOfColumns; i++) {
+      const col = data.map(x => x[i])
+      meanvals.push(mean(col))
+      stdvals.push(standardDeviation(col))
+    }
+    const standardized = []
+    for (var i = 0; i < numberOfRows; i++) {
+      const row = []
+      for (var j = 0; j < numberOfColumns; j++) {
+        row.push((data[i][j] - meanvals[j])/(stdvals[j]))
+      }
+      standardized.push(row)
+    }
+    return standardized
+  }
+
+  async function train(data) {
+    console.log(data);
+    let dataset = data.map(x => Object.values(x).map(Number));
+    dataset = shuffle(dataset)
+    const test_train_split = 0.2;
+    const [features, targets] = getFeatureTargetSplit(dataset)
+    const normalizedFeatures = normalizeData(features)
+    const standardizedFeatures = standardizeData(features)
+    const [x_train, x_test, y_train, y_test] = getTestTrainSplit(standardizedFeatures, targets, test_train_split)
+    const tensors = convertToTensors(x_train, x_test, y_train, y_test);
+    console.log("x train", x_train)
+    console.log("x test", x_test)
+    console.log("y train", y_train)
+    console.log("y test", y_test)
+    
     const model = await trainModel(
       tensors.trainFeatures,
       tensors.trainTargets,
@@ -144,7 +184,7 @@ const TrainModel = ({ match }) => {
     );
     const predictions = model.predict(tensors.testFeatures)
     console.log("PREDICT", predictions);
-    console.log("R2 score: ", getRSquared(predictions.arraySync(), y_test))
+    console.log("R2 score: ", getR2Score(predictions.arraySync(), y_test))
   }
 
   async function trainModel(xTrain, yTrain, xTest, yTest) {
