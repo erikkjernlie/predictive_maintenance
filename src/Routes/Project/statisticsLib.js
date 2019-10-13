d6b138247ee33fa34bd95e42636ea67762911e32
Herman Wika Horn <herman.wika.horn@entur.org>
2019-10-13 20:47:06 +0200

    moved some statistics functions to separate file

---
+++ src/Routes/Project/statisticsLib.js
@@ -0,0 +1,130 @@
+import {
+    shuffle,
+    sampleCovariance,
+    max,
+    min,
+    sum,
+    mean,
+    standardDeviation,
+    sampleStandardDeviation,
+    sampleCorrelation
+  } from 'simple-statistics'
+
+export function getDatasetByColumns(dataset) {
+    const numberOfColumns = dataset[0].length
+    const numberOfRows = dataset.length
+    const columnsData = []
+    for (var i = 0; i < numberOfColumns; i++) {
+      const column = dataset.map(x => x[i])
+      columnsData.push(column)
+    }
+    return columnsData
+  }
+
+export function getCovarianceMatrix(dataset) {
+    const columnData = getDatasetByColumns(dataset)
+    const numberOfColumns = columnData.length
+    const covariances = []
+    for (var i = 0; i < numberOfColumns; i++) {
+      const covariances_column_i = []
+      for (var j = 0; j < numberOfColumns; j++) {
+        covariances_column_i.push(sampleCorrelation(columnData[i], columnData[j]))
+      }
+      covariances.push(covariances_column_i)
+    }
+    return covariances
+  }
+
+export function discardCovariantColumns(dataset, cov) {
+    for (var i = 0; i < dataset[0].length; i++) {
+      for (var j = i+1; j < dataset[0].length; j++) {
+        if (cov[i][j] > 0.95) {
+          dataset = dataset.map(x => x.splice(i))
+          break
+        }
+      }
+    }
+    console.log("Data after discarding column", dataset)
+    return dataset
+  }
+
+export function standardizeData(dataset) {
+    const numberOfColumns = dataset[0].length
+    const numberOfRows = dataset.length
+    let meanvals = []
+    let stdvals = []
+    for (var i = 0; i < numberOfColumns; i++) {
+      const col = dataset.map(x => x[i])
+      meanvals.push(mean(col))
+      stdvals.push(standardDeviation(col))
+    }
+    const standardized = []
+    for (var i = 0; i < numberOfRows; i++) {
+      const row = []
+      for (var j = 0; j < numberOfColumns; j++) {
+        row.push((dataset[i][j] - meanvals[j])/(stdvals[j]))
+      }
+      standardized.push(row)
+    }
+    return standardized
+  }
+
+export function normalizeData(data) {
+    const numberOfColumns = data[0].length
+    const numberOfRows = data.length
+    let maxvals = []
+    let minvals = []
+    for (var i = 0; i < numberOfColumns; i++) {
+      const col = data.map(x => x[i])
+      maxvals.push(max(col))
+      minvals.push(min(col))
+    }
+    const normalized = []
+    for (var i = 0; i < numberOfRows; i++) {
+      const row = []
+      for (var j = 0; j < numberOfColumns; j++) {
+        row.push((data[i][j] - minvals[j])/(maxvals[j]-minvals[j]))
+      }
+      normalized.push(row)
+    }
+    return normalized
+  }
+
+export function getR2Score(predict, data) {
+    console.log("Inside getRSquared function")
+    console.log(predict)
+    console.log(data)
+    data = data.map(x => Number(x));
+    predict = predict.map(x => Number(x))
+
+    var meanValue = 0; // MEAN VALUE
+    var SStot = 0; // THE TOTAL SUM OF THE SQUARES
+    var SSres = 0; // RESIDUAL SUM OF SQUARES
+    var rSquared = 0;
+
+    // SUM ALL VALUES
+    for (var n = 0; n < data.length; n++) { 
+      meanValue += data[n];
+    }
+    // GET MEAN VALUE 
+    meanValue = (meanValue / data.length);
+
+    for (var n = 0; n < data.length; n++) {
+      // CALCULATE THE SSTOTAL
+      SStot += Math.pow(data[n] - meanValue, 2); 
+      // CALCULATE THE SSRES
+      SSres += Math.pow(predict[n] - data[n], 2);
+    }
+
+    // R SQUARED
+    rSquared = 1 - (SSres / SStot);
+    
+    return {
+        meanValue: meanValue,
+        SStot: SStot,
+        SSres: SSres,
+        rSquared: rSquared
+    };
+  }
+
+export default getR2Score
